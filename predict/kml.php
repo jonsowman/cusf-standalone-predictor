14c98a5a937e928a631e34cf9f67e57dce0d7cbf
Jon Sowman <jon.sowman@gmail.com>
2010-07-30 01:26:36 -0700

    added defines to kml generation script

--- predict/kml.php
+++ predict/kml.php
@@ -26,8 +26,8 @@ $uuid = $_GET['uuid'];
 if ( strlen($uuid) != 40 || !ctype_alnum($uuid) ) die("The supplied UUID was not a valid SHA1 hash");
 
 // Construct paths to required predictor files and check they exist
-$flight_csv = $c_preds_path . $uuid . "/" . $c_flight_csv;
-$scenario_file = $c_preds_path . $uuid . "/" . $c_scenario_file;
+$flight_csv = PREDS_PATH . $uuid . "/" . FLIGHT_CSV;
+$scenario_file = PREDS_PATH . $uuid . "/" . SCENARIO_FILE;
 if ( !file_exists( $flight_csv ) || !file_exists( $scenario_file ) ) die("No prediction data for UUID");
 
 // make the prediction model
