b24f515a2ca8937d34b55f0569903bf6bcc0d57a
jonsowman <jon@hexoc.com>
2010-08-03 08:30:52 +0000

    More useful cronjob output

--- cron/prune-predictions-cronjob.sh
+++ cron/prune-predictions-cronjob.sh
@@ -10,5 +10,5 @@ echo `ls $REPOROOT$DATADIR/ -l | wc -l` "prediction scenarios found"
 echo `find $REPOROOT$DATADIR/* -maxdepth 0 -$PARAM +$AGE | wc -l` "of them had $PARAM of more than $AGE days"
 echo "Now deleting..."
 find $REPOROOT$DATADIR/* -maxdepth 0 -$PARAM +$AGE -exec rm -rf {} \;
-echo "DONE"
-
+echo "Done deleting."
+echo `ls $REPOROOT$DATADIR/ -l | wc -l` "prediction scenarios remaining"
